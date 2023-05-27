9254a83463ed5bae8c86035bb433d505e6ae302a
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-05-28 00:28:29 +0700

    CRUD - Display Data Users in View(READ)

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,10 @@ const handleLoginApi = (userEmail, userPassword) => {
   return axios.post('/api/login', {email: userEmail, password: userPassword});
 }
 
-export { handleLoginApi}
+const getAllUsers = (inputId) =>{
+  return axios.get(`/api/get-all-users?id=${inputId}`, {
+    id: inputId
+  })
+}
+
+export { handleLoginApi, getAllUsers}
