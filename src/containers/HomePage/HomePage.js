0db0bcb0a99cb290a1fc61ba9be513e73e1723d0
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-10-02 22:34:40 +0700

    React-Slider-Carousel in  React App

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
+import Specialty from './Section/Specialty';
 
 class HomePage extends Component {
 
     render() {
         return (
             <div>
-                <HomeHeader/>
+                <HomeHeader />
+                <Specialty />
+                <div style={{ height: '300px' }}></div>
             </div>
         );
     }
