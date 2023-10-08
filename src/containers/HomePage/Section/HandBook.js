8f8a1e43ec4d3deb21faba79bffbbd8283a7b304
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-10-08 22:17:44 +0700

    #50 Fix bug FE Interface

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -3,43 +3,44 @@ import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import Slider from "react-slick";
 
-class Specialty extends Component {
+class HandBook extends Component {
     render() {
         return (
-            <div className='section-share section-specialty'>
+            <div className='section-share section-handbook'>
                 <div className='section-container'>
                     <div className='section-header'>
-                        <span className='title-section'>Chuyên khoa phổ biển</span>
+                        <span className='title-section'>Cẩm nang</span>
                         <button className='btn-section'>Xem thêm</button>
                     </div>
+
                     <div className='section-body'>
                         <Slider {...this.props.settings}>
                             <div className='section-customize'>
-                                <div className="bg-image section-specialty"/>
+                                <div className="bg-image section-handbook"/>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className="bg-image section-specialty"/>
+                                <div className="bg-image section-handbook"/>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className="bg-image section-specialty"/>
+                                <div className="bg-image section-handbook"/>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className="bg-image section-specialty"/>
+                                <div className="bg-image section-handbook"/>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className="bg-image section-specialty"/>
+                                <div className="bg-image section-handbook"/>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className="bg-image section-specialty"/>
+                                <div className="bg-image section-handbook"/>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className="bg-image section-specialty"/>
+                                <div className="bg-image section-handbook"/>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                         </Slider>
@@ -63,4 +64,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
