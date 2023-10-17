36742f7d159a39f04bdd81fba1eed720eafc5fce
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-10-17 21:16:35 +0700

    Redux - React app

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -11,12 +11,17 @@ class About extends Component {
         </div>
         <div className='section-about-content'>
           <div className='content-left'>
-            <iframe width="100%" height="400px" src="https://www.youtube.com/embed/vdk4Kpkwdkc?si=FgyTkYW3G4Ewkga0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="100%" height="400px"
+              src="https://www.youtube.com/embed/vdk4Kpkwdkc?si=FgyTkYW3G4Ewkga0"
+              title="YouTube video player"
+              frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
           </div>
           <div className='content-right'>
             <p>
-            Khám bệnh không phải xếp hàng ở Hà Nội
-            Với ứng dụng trên điện thoại, người bệnh đặt lịch khám, chọn bác sĩ ở 60 bệnh viện, phòng khám tại Hà Nội đồng thời nhận được thông báo mức chi phí.
+              Khám bệnh không phải xếp hàng ở Hà Nội
+              Với ứng dụng trên điện thoại, người bệnh đặt lịch khám, chọn bác sĩ ở 60 bệnh viện, phòng khám tại Hà Nội đồng thời nhận được thông báo mức chi phí.
             </p>
           </div>
         </div>
