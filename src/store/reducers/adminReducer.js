62ad228f15849071886670f1fb1b037197d22c57
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-10-22 21:19:47 +0700

    Using Redux #57

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,33 +1,30 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    isLoggedIn: false,
-    adminInfo: null
+  genders: [],
+  roles: [],
+  positions: []
 }
 
-const appReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.ADMIN_LOGIN_SUCCESS:
-            return {
-                ...state,
-                isLoggedIn: true,
-                adminInfo: action.adminInfo
-            }
-        case actionTypes.ADMIN_LOGIN_FAIL:
-            return {
-                ...state,
-                isLoggedIn: false,
-                adminInfo: null
-            }
-        case actionTypes.PROCESS_LOGOUT:
-            return {
-                ...state,
-                isLoggedIn: false,
-                adminInfo: null
-            }
-        default:
-            return state;
-    }
+const adminReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_GENDER_START:
+      return {
+        ...state
+      }
+    case actionTypes.FETCH_GENDER_SUCCESS:
+      let copyState = { ...state };
+      copyState.genders = action.data;
+      return {
+        ...copyState
+      }
+    case actionTypes.FETCH_GENDER_FAILED:
+      return {
+        ...state
+      }
+    default:
+      return state;
+  }
 }
 
-export default appReducer;
+export default adminReducer;
