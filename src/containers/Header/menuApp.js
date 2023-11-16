26afde6b4753422906bf6a512b8662ba76afd889
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-11-16 22:07:51 +0700

    Create User Permissions Menu When Login #71

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,10 +1,10 @@
 export const adminMenu = [
     { //Quan ly nguoi dung
-        name: 'menu.admin.manage-user', 
+        name: 'menu.admin.manage-user',
         menus: [
             {
                 name: 'menu.admin.crud', link: '/system/user-manage'
-            }, 
+            },
             {
                 name: 'menu.admin.crud-redux', link: '/system/user-redux'
             },
@@ -15,36 +15,50 @@ export const adminMenu = [
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ]
             },
-            {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            // {
+            //     name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            // }
+            { //Quan ly ke hoach kham benh bacsi
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
             }
         ]
     },
     { //Quan ly Phong kham
-        name: 'menu.admin.clinic', 
+        name: 'menu.admin.clinic',
         menus: [
             {
                 name: 'menu.admin.manage-clinic', link: '/system/manage-clinic'
-            }, 
-            
+            },
+
         ]
     },
     { //Quan ly Chuyên Khoa
-        name: 'menu.admin.specialty', 
+        name: 'menu.admin.specialty',
         menus: [
             {
                 name: 'menu.admin.manage-specialty', link: '/system/manage-specialty'
-            }, 
-            
+            },
+
         ]
     },
     { //Quan ly Cam nang
-        name: 'menu.admin.handbook', 
+        name: 'menu.admin.handbook',
         menus: [
             {
                 name: 'menu.admin.manage-handbook', link: '/system/manage-handbook'
-            }, 
-            
+            },
+
         ]
     },
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+            { //Quan ly ke hoach kham benh bacsi
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            }
+        ]
+    }
 ];
