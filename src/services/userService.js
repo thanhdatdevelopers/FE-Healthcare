4636e393fc8f16a417ce951f587cfb8f48460418
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-11-24 23:22:12 +0700

    BulkCreate With Sequelize - Save Array of Medical Exam Schedule Data #74

--- src/services/userService.js
+++ src/services/userService.js
@@ -46,6 +46,10 @@ const getDetailInforDoctor = (inputId) => {
   return axios.get(`/api/get-detail-doctor-by-id?id=${inputId}`)
 }
 
+const saveBulkScheduleDoctor = (data) => {
+  return axios.post('/api/bulk-create-schedule', data)
+}
+
 export { 
   handleLoginApi, 
   getAllUsers, 
@@ -56,5 +60,6 @@ export {
   getTopDoctorHomeService,
   getAllDoctors,
   saveDetailDoctorService,
-  getDetailInforDoctor
+  getDetailInforDoctor,
+  saveBulkScheduleDoctor
 }
