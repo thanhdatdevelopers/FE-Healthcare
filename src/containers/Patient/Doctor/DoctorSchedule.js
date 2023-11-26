b012f25718f267d244b993138b445c38dd3cb1b8
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-11-26 09:07:54 +0700

    Dynamically Load Data Select Medical Examination Schedule By Time Date Created #76

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -11,6 +11,7 @@ class DoctorSchedule extends Component {
     super(props);
     this.state = {
       allDays: [],
+      allAvailableTime: []
     }
   }
 
@@ -19,12 +20,17 @@ class DoctorSchedule extends Component {
     this.setArrDays(language)
   }
 
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   setArrDays = (language) => {
     let allDays = []
     for (let i = 0; i < 7; i++) {
       let object = {}
       if (language === LANGUAGES.VI) {
-        object.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+        let labelVi = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+        object.label = this.capitalizeFirstLetter(labelVi)
       } else {
         object.label = moment(new Date()).add(i, 'days').locale('en').format('ddd - DD/MM');
       }
@@ -48,12 +54,19 @@ class DoctorSchedule extends Component {
       let doctorId = this.props.doctorIdFromParent;
       let date = event.target.value
       let res = await getScheduleDoctorByDate(doctorId, date)
+
+      if (res && res.errCode === 0) {
+        this.setState({
+          allAvailableTime: res.data ? res.data : []
+        })
+      }
       console.log('res', res);
     }
   }
 
   render() {
-    let { allDays } = this.state
+    let { allDays, allAvailableTime } = this.state
+    let { language } = this.props
     return (
       <div className='doctor-schedule-container'>
         <div className='all-schedule'>
@@ -75,7 +88,22 @@ class DoctorSchedule extends Component {
           </select>
         </div>
         <div className='all-available-time'>
-
+          <div className='text-calendar'>
+            <i className='fas fa-calendar-alt'><span>Lich Kham</span></i>
+          </div>
+          <div className='time-content'>
+            {allAvailableTime && allAvailableTime.length > 0 ?
+              allAvailableTime.map((item, index) => {
+                let timeDisplay = language === LANGUAGES.VI ?
+                  item.timeTypeData.valueVi : item.timeTypeData.valueEn
+                return (
+                  <button key={index}>{timeDisplay}</button>
+                )
+              })
+              : 
+              <div>Bac si khong co lich hen trong tgian nay, vui long chon tgain khac</div>
+            }
+          </div>
         </div>
       </div>
     );
