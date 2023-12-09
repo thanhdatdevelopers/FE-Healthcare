f841831a38dca5b40dfd556dd95693b2d83f7319
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-12-09 16:47:16 +0700

    API to Dynamically Load Doctor Information #83 --- IMAGE & TEXT

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { FormattedMessage } from 'react-intl';
 import './BookingModal.scss';
 import { Modal } from 'reactstrap';
+import ProfileDoctor from "../ProfileDoctor"
+import _ from 'lodash';
 
 class BookingModal extends Component {
   constructor(props) {
@@ -24,7 +26,11 @@ class BookingModal extends Component {
   }
 
   render() {
-    let { isOpenModal, closeBookingClose, dateTime } = this.props
+    let { isOpenModal, closeBookingClose, dataTime } = this.props
+    let doctorId = '';
+    if (dataTime && !_.isEmpty(dataTime)) {
+      doctorId = dataTime.doctorId
+    }
     return (
       <Modal
         isOpen={isOpenModal}
@@ -42,10 +48,12 @@ class BookingModal extends Component {
           </div>
           <div className='booking-modal-body'>
             <div className='doctor-infor'>
-
+              <ProfileDoctor
+                doctorId={doctorId}
+              />
             </div>
             <div className='price'>
-              Gia kham 500k
+              Gia kham 500.000 VND
             </div>
             <div className='row'>
               <div className='col-6 form-group'>
