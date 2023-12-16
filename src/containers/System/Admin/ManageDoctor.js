9a65af0550e39f3f8dfeea54f159b7701942a249
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-12-16 23:16:59 +0700

    Add Component Details of Medical Examination Specialties #92

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -27,12 +27,18 @@ class ManageDoctor extends Component {
       listPrice: [],
       listPayment: [],
       listProvince: [],
+      listClinic: [],
+      listSpecialty: [],
       selectedPrice: '',
       selectedPayment: '',
       selectedProvince: '',
+      selectedClinic: '',
+      selectedSpecialty: '',
       nameClinic: '',
       addressClinic: '',
-      note: ''
+      note: '',
+      clinicId: '',
+      specialtyId: ''
     }
   }
 
@@ -75,6 +81,14 @@ class ManageDoctor extends Component {
           result.push(object)
         })
       }
+      if (type === 'SPECIALTY') {
+        inputData.map((item, index) => {
+          let object = {}
+          object.label = item.name;
+          object.value = item.id;
+          result.push(object)
+        })
+      }
     }
     return result
   }
@@ -89,16 +103,18 @@ class ManageDoctor extends Component {
 
 
     if (prevProps.allRequiredDoctorInfor !== this.props.allRequiredDoctorInfor) {
-      let { resPayment, resPrice, resProvince } = this.props.allRequiredDoctorInfor;
+      let { resPayment, resPrice, resProvince, resSpecialty } = this.props.allRequiredDoctorInfor;
 
       let dataSelectPrice = this.buildDataInputSelect(resPrice, 'PRICE')
       let dataSelectPayment = this.buildDataInputSelect(resPayment, 'PAYMENT')
       let dataSelectProvince = this.buildDataInputSelect(resProvince, 'PROVINCE')
+      let dataSelectSpecialty = this.buildDataInputSelect(resSpecialty, 'SPECIALTY')
 
       this.setState({
         listPrice: dataSelectPrice,
         listPayment: dataSelectPayment,
         listProvince: dataSelectProvince,
+        listSpecialty: dataSelectSpecialty,
       })
     }
 
@@ -139,7 +155,9 @@ class ManageDoctor extends Component {
       selectedProvince: this.state.selectedProvince.value,
       nameClinic: this.state.nameClinic,
       addressClinic: this.state.addressClinic,
-      note: this.state.note
+      note: this.state.note,
+      clinicId: this.state.selectedClinic && this.state.selectedClinic.value ? this.state.selectedClinic.value : '',
+      specialtyId: this.state.selectedSpecialty.value
     })
   }
 
@@ -315,9 +333,36 @@ class ManageDoctor extends Component {
           </div>
         </div>
 
+        <div className='row'>
+          <div className='col-4 form-group'>
+            <label>
+              <FormattedMessage id="admin.manage-doctor.specialty" />
+            </label>
+            <Select
+              value={this.state.selectedSpecialty}
+              onChange={this.handleChangeSelectDoctorInfor}
+              options={this.state.listSpecialty}
+              placeholder={<FormattedMessage id="admin.manage-doctor.specialty" />}
+              name="selectedSpecialty"
+            />
+          </div>
+          <div className='col-4 form-group'>
+            <label>
+              <FormattedMessage id="admin.manage-doctor.select-clinic" />
+            </label>
+            <Select
+              value={this.state.selectedClinic}
+              onChange={this.handleChangeSelectDoctorInfor}
+              options={this.state.listClinic}
+              placeholder={<FormattedMessage id="admin.manage-doctor.select-clinic" />}
+              name="selectedClinic"
+            />
+          </div>
+        </div>
+
         <div className='manage-doctor-editor'>
           <MdEditor
-            style={{ height: '500px' }}
+            style={{ height: '300px' }}
             renderHTML={text => mdParser.render(text)}
             onChange={this.handleEditorChange}
             value={this.state.contentMarkdown}
--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -4,7 +4,8 @@ import {
   createNewUserService, getAllUsers,
   deleteUserService, editUserService,
   getTopDoctorHomeService,
-  getAllDoctors, saveDetailDoctorService
+  getAllDoctors, saveDetailDoctorService,
+  getAllSpecialty
 } from '../../services/userService';
 import { toast } from 'react-toastify';
 
@@ -304,14 +305,18 @@ export const getRequiredDoctorInfor = () => {
       let resPrice = await getAllCodeService('PRICE')
       let resPayment = await getAllCodeService('PAYMENT')
       let resProvince = await getAllCodeService('PROVINCE')
+      let resSpecialty = await getAllCodeService()
       
       if (resPrice && resPrice.errCode === 0
         && resPayment && resPayment.errCode === 0
-        && resProvince && resProvince.errCode === 0) {
+        && resProvince && resProvince.errCode === 0 
+        && resSpecialty && resSpecialty.errCode === 0)
+        {
           let data = {
             resPrice: resPrice.data,
             resPayment: resPayment.data,
-            resProvince: resProvince.data
+            resProvince: resProvince.data,
+            resSpecialty: resSpecialty.data
           }
         dispatch(fetchRequiredDoctorInforSuccess(data))
       } else {
