afdd9c6549e0983286af562094d903a38455b018
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2023-12-16 23:28:41 +0700

    Design Interface Function View Details of Specialties #93

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -3,12 +3,15 @@ import { connect } from "react-redux";
 import { FormattedMessage } from 'react-intl';
 import './DetailSpecialty.scss'
 import HomeHeader from '../../HomePage/HomeHeader'
+import DoctorSchedule from '../Doctor/DoctorSchedule'
+import DoctorExtrainfor from '../Doctor/DoctorExtrainfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor'
 
 class DetailSpecialty extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      arrDoctorId: [44, 45, 46]
     }
   }
 
@@ -22,10 +25,43 @@ class DetailSpecialty extends Component {
   }
 
   render() {
+    let { arrDoctorId } = this.state
+
     return (
-      <>
-        <HomeHe />
-      </>
+      <div className='detail-specialty-container'>
+        <HomeHeader />
+        <div className='detail-specialty-body'>
+
+        </div>
+        {arrDoctorId && arrDoctorId.length > 0 &&
+          arrDoctorId.map((item, index) => {
+            return (
+              <div className='each-doctor' key={index}>
+                <div className='dt-content-left'>
+                  <div className='profile-doctor'>
+                    <ProfileDoctor
+                      doctorId = {item}
+                      isShowDescriptionDoctor = {true}
+                    />
+                  </div>
+                </div>
+                <div className='dt-content-right'>
+                  <div className='doctor-schedule'>
+                    <DoctorSchedule
+                      doctorIdFromParent = {item}
+                    />
+                  </div>
+                  <div className='doctor-extra-infor'>
+                    <DoctorExtrainfor
+                      doctorIdFromParent = {item}
+                    />
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     )
   }
 }
